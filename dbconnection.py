75fc283a8f294ba0bcdbdd159deef2d17f31d0a8
iruy-fr <iruy.fr@gmail.com>
2024-11-08 20:36:51 -0300

    Criando automação

--- dbconnection.py
+++ dbconnection.py
@@ -3,13 +3,13 @@ import os
 
 
 def readpath():
-    filepath = open(file=f"{os.path.dirname(os.path.realpath(__name__))}\oracledb.txt", encoding='utf-8', mode='r')
+    filepath = open(file=f"{os.path.dirname(os.path.realpath(__name__))}/oracledb.txt", encoding='utf-8', mode='r')
     pwdfile = filepath.read()
     return pwdfile
 
 
 def via_db():
-    db.init_oracle_client(lib_dir=f"{os.path.dirname(os.path.realpath(__name__))}\_internal\integraPratic\instantclient_23_6")
+    db.init_oracle_client(lib_dir=f"{os.path.dirname(os.path.realpath(__name__))}/_internal/integraPratic/instantclient_23_6")
     connection = db.connect(
         user='VIASOFT',
         password=readpath(),
